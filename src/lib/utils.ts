01f5725391ff8c7130b2530558771a5738102c01
Gleidson Jonas <gleidsonjonas@hotmail.com>
2025-05-27 23:21:39 -0300

    FIX: model memory

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,14 @@
+import { Message } from "ai"
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+}
+
+export function getPreviousMessages(messages: Message[]) {
+  return messages
+    .slice(0, messages.length - 1)
+    .map((e: any, i: number) => `indext: ${i}; role: ${e.role}; input: ${e.content};`)
+    .join("\n");
 }
